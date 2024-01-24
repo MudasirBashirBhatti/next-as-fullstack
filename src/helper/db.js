51618874b16583d566baae246792fbb79d29382d
Mudasir <for.all6474@gmail.com>
2024-01-24 13:08:52 +0500

    database connected successfully

---
+++ src/helper/db.js
@@ -0,0 +1,18 @@
+//1. import mongoose
+import mongoose from "mongoose";
+
+// 2. create async function
+export const connectDb = async () => {
+  //3. use try and catch method to catch error if any
+  try {
+    // here {connection} is to get all the methods of mongoose
+    const { connection } = await mongoose.connect(process.env.MONGO_DB_URL, {
+      dbName: "work_manager",
+    });
+    console.log("db connected....");
+    // console.log(connection);
+  } catch (error) {
+    console.log("error_occured...");
+    console.log(error);
+  }
+};
