96481001135687c7ad28e142f0d0c3dd6db30d9e
Mudasir <for.all6474@gmail.com>
2024-01-29 08:52:45 +0500

    purt request api to update users work

--- src/app/apis/work/[workId]/route.js
+++ src/app/apis/work/[workId]/route.js
@@ -1,10 +1,36 @@
 import { Work } from "@/models/work.model";
 import { NextResponse } from "next/server";
 
+// get single user work by his id
 export async function GET(request, { params }) {
   const { workId } = params;
   try {
     const getSingleWork = await Work.findById(workId);
     return NextResponse.json(getSingleWork);
-  } catch (error) {}
+  } catch (error) {
+    return NextResponse.json({
+      message: "failed to get users's work",
+      success: "false",
+    });
+  }
+}
+
+// update single user work by his id
+export async function PUT(request, { params }) {
+  const { workId } = params;
+  const { title, content, status } = await request.json();
+  try {
+    const work = await Work.findById(workId);
+    work.title = title;
+    work.content = content;
+    work.status = status;
+
+    const updatedWork = await work.save();
+    return NextResponse.json(updatedWork);
+  } catch (error) {
+    return NextResponse.json({
+      message: "failed to updated user's work",
+      success: "true",
+    });
+  }
 }
